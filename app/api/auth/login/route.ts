4cbbbc19a353c045bb58797bbf9eee7478ef42d8
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-09-29 20:04:51 +0600

    The user login endpoint has been created

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,69 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { NextResponse } from "next/server";
+
+import connectMongodb from "@/libs/connect_mongodb";
+import User from "@/models/users.model";
+
+export async function POST(request: Request) {
+    const { email, password } = await request.json()
+
+    try {
+        await connectMongodb();
+        const isEmailExist = await User.findOne({ email });
+
+        if (!isEmailExist) {
+            return NextResponse.json({
+                success: false,
+                message: 'Account does not exist.'
+            }, { status: 400 });
+        };
+
+        if (isEmailExist.isBlocked) {
+            return NextResponse.json({
+                success: false,
+                message: 'Account Blocked'
+            }, { status: 400 });
+        };
+
+        const checkValidPassword = await bcrypt.compare(password, isEmailExist.password);
+
+        if (!checkValidPassword) {
+            return NextResponse.json({
+                success: false,
+                message: 'Wrong Password'
+            }, { status: 500 });
+        };
+
+        const accessToken = jwt.sign({
+            email: isEmailExist.email,
+            username: isEmailExist.username,
+            role: isEmailExist.role
+        }, process.env.JWT_ACCESS_TOKEN_SECRET as string, {
+            expiresIn: '1d'
+        });
+
+        const refreshToken = jwt.sign({
+            email: isEmailExist.email,
+            username: isEmailExist.username,
+            role: isEmailExist.role
+        }, process.env.JWT_REFRESH_TOKEN_SECRET as string, {
+            expiresIn: '30d'
+        });
+
+        return NextResponse.json({
+            success: true,
+            message: 'Account Logged in Success',
+            accessToken,
+            refreshToken
+        }, { status: 200 });
+
+    } catch (error) {
+        console.log(error);
+
+        return NextResponse.json({
+            success: false,
+            message: 'Something went wrong'
+        }, { status: 500 });
+    }
+}
--- interface/users.interface.ts
+++ interface/users.interface.ts
@@ -8,6 +8,7 @@ export interface IUser extends Document {
     isEmailVerified: boolean;
     role: 'user' | 'admin';
     isPremiumMember: boolean;
+    isBlocked: boolean;
     followers: string[];
     following: string[];
     createdAt?: Date;
