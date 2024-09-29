6a67d098d23258152a10eb5d39cad1fa05e80e3d
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-09-29 18:54:35 +0600

    default layout created

--- app/(others_layout)/auth/login/page.tsx
+++ app/(others_layout)/auth/login/page.tsx
@@ -5,7 +5,7 @@ import { Button, Card, CardBody, CardHeader, Input, Link } from "@nextui-org/rea
 export default function Login() {
 
     return (
-        <div className="min-h-screen flex justify-center items-center">
+        <div className="min-h-screen flex justify-center items-center fixed inset-0 px-5">
             <Card className="w-full max-w-2xl mx-auto">
                 <CardHeader className="flex flex-col gap-1 px-6 py-5">
                     <h1 className="text-2xl font-bold">Login</h1>
@@ -13,9 +13,9 @@ export default function Login() {
                 </CardHeader>
                 <CardBody className="px-6 py-5">
                     <form className="space-y-5">
-                        <Input label="Email" size='sm' type="email" />
-                        <Input label="Password" size='sm' type="password" />
-                        <Button className='w-full rounded-lg py-6'>Sign In</Button>
+                        <Input required label="Email" size='sm' type="email" />
+                        <Input required label="Password" size='sm' type="password" />
+                        <Button className='w-full rounded-lg py-6' type="submit">Sign In</Button>
                     </form>
                     <div className="flex justify-center mt-6">
                         {/* <Link className='text-default-foreground' href="#" size="sm">
@@ -23,7 +23,7 @@ export default function Login() {
                     </Link> */}
 
                         <div className="">
-                            Don&apos;t have account?  <Link className='' href="/auth/register">
+                            Don&apos;t have account ?  <Link className='' href="/auth/register">
                                 Register Now
                             </Link>
                         </div>
