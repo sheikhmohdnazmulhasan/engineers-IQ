c1e58308fcd67a6eb2078d49f6e5fb2732db207b
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-09-29 11:28:10 +0600

    The user's avatar is shown in the navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   Navbar as NextUINavbar,
   NavbarContent,
@@ -13,10 +15,12 @@ import { Input } from "@nextui-org/input";
 import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar, User } from "@nextui-org/react";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 import { SearchIcon, Logo } from "@/components/icons";
+
 
 export const Navbar = () => {
   const searchInput = (
@@ -39,6 +43,78 @@ export const Navbar = () => {
       type="search"
     />
   );
+
+  const profileDropdownDesktop = (
+    <Dropdown placement="bottom-start">
+      <DropdownTrigger>
+        <User
+          as="button"
+          avatarProps={{
+            isBordered: true,
+            src: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
+          }}
+          className="transition-transform"
+          description="@tonyreichert"
+          name="Tony Reichert"
+        />
+      </DropdownTrigger>
+      <DropdownMenu aria-label="User Actions" variant="flat">
+        <DropdownItem key="profile" className="h-14 gap-2">
+          <p className="font-bold">Signed in as</p>
+          <p className="font-bold">@tonyreichert</p>
+        </DropdownItem>
+        <DropdownItem key="settings">
+          My Settings
+        </DropdownItem>
+        <DropdownItem key="team_settings">Team Settings</DropdownItem>
+        <DropdownItem key="analytics">
+          Analytics
+        </DropdownItem>
+        <DropdownItem key="system">System</DropdownItem>
+        <DropdownItem key="configurations">Configurations</DropdownItem>
+        <DropdownItem key="help_and_feedback">
+          Help & Feedback
+        </DropdownItem>
+        <DropdownItem key="logout" color="danger">
+          Log Out
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+
+  const profileDropdownMobile = (
+    <Dropdown placement="bottom-end">
+      <DropdownTrigger>
+        <Avatar
+          isBordered
+          as="button"
+          className="transition-transform"
+          src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+        />
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Profile Actions" variant="flat">
+        <DropdownItem key="profile" className="h-14 gap-2">
+          <p className="font-semibold">Signed in as</p>
+          <p className="font-semibold">zoey@example.com</p>
+        </DropdownItem>
+        <DropdownItem key="settings">
+          My Settings
+        </DropdownItem>
+        <DropdownItem key="team_settings">Team Settings</DropdownItem>
+        <DropdownItem key="analytics">
+          Analytics
+        </DropdownItem>
+        <DropdownItem key="system">System</DropdownItem>
+        <DropdownItem key="configurations">Configurations</DropdownItem>
+        <DropdownItem key="help_and_feedback">
+          Help & Feedback
+        </DropdownItem>
+        <DropdownItem key="logout" color="danger">
+          Log Out
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  )
 
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
@@ -75,10 +151,12 @@ export const Navbar = () => {
         <NavbarItem className="hidden lg:flex">
           {searchInput}
         </NavbarItem>
+        {profileDropdownDesktop}
       </NavbarContent>
 
       {/* mobile */}
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+        {profileDropdownMobile}
         <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
