e7eb17920f4fb558c4b95878d96f0731f6f9f105
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-09-29 21:16:10 +0600

    zod schema created for login form validation

--- app/(others_layout)/auth/login/page.tsx
+++ app/(others_layout)/auth/login/page.tsx
@@ -1,8 +1,15 @@
 'use client'
 
 import { Button, Card, CardBody, CardHeader, Input, Link } from "@nextui-org/react"
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
 
 export default function Login() {
+    const { handleSubmit, register } = useForm();
+
+    const handleLogin: SubmitHandler<FieldValues> = async (data) => {
+
+        console.log(data);
+    }
 
     return (
         <div className="min-h-screen flex justify-center items-center fixed inset-0 px-5">
@@ -12,9 +19,9 @@ export default function Login() {
                     <p className="text-sm text-default-500">Enter your credentials to access your account</p>
                 </CardHeader>
                 <CardBody className="px-6 py-5">
-                    <form className="space-y-5">
-                        <Input required label="Email" size='sm' type="email" />
-                        <Input required label="Password" size='sm' type="password" />
+                    <form className="space-y-5" onSubmit={handleSubmit(handleLogin)}>
+                        <Input required label="Email" size='sm' type="email" {...register('email')} />
+                        <Input required label="Password" size='sm' type="password" {...register('password')} />
                         <Button className='w-full rounded-lg py-6' type="submit">Sign In</Button>
                     </form>
                     <div className="flex justify-center mt-6">
