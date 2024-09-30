744b01c11f40d83899630833ba386a9f0049ecb2
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-09-30 13:59:37 +0600

    server action created for register user

---
+++ services/auth.services.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use server'
+
+import { FieldValues } from "react-hook-form"
+
+import axiosInstance from "@/libs/axiosInstance"
+
+export const registerUser = async (userData: FieldValues) => {
+
+    try {
+        const { data } = await axiosInstance.post('/auth/register', userData);
+        return data;
+
+    } catch (error: any) {
+        throw new Error(error)
+    }
+} 
