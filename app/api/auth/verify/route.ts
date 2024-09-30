c1614ced494f5979995a7134c0fecd8dc4fb3bff
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-09-30 21:18:28 +0600

    Account verification is handled in the backend

--- app/api/auth/verify/route.ts
+++ app/api/auth/verify/route.ts
@@ -1,32 +1,50 @@
 import { NextResponse } from "next/server";
 
 import { decrypt } from '@/utils/text_encryptor';
-
+import User from "@/models/users.model";
+import connectMongodb from "@/libs/connect_mongodb";
 
 export async function PATCH(request: Request) {
 
-    const { token } = await request.json();
+    try {
+        await connectMongodb();
+        const { token } = await request.json();
 
-    if (!token) {
+        if (!token) {
+            return NextResponse.json({
+                message: 'something wrong'
+            }, { status: 400 });
+        }
+
+        const decryptedData = decrypt(token);
+        if (!decryptedData) {
+            return NextResponse.json({
+                message: 'something wrong'
+            }, { status: 400 });
+        }
+
+        console.log(decryptedData);
+
+        const spited = decryptedData.split('+++');
+        const currentTime = new Date();
+        const expiry = new Date(spited[1]);
+
+        if (currentTime > expiry) {
+            return NextResponse.json({
+                message: 'something wrong'
+            }, { status: 400 });
+        }
+
+        const updateUser = await User.findOneAndUpdate({ email: spited[0] }, { isEmailVerified: true });
+
+        if (updateUser) {
+            return NextResponse.json({
+                message: 'operation successful'
+            }, { status: 400 });
+        }
+    } catch (error) {
         return NextResponse.json({
             message: 'something wrong'
-        }, { status: 400 });
+        }, { status: 500 });
     }
-
-    const decryptedData = decrypt(token);
-    if (!decryptedData) {
-        return NextResponse.json({
-            message: 'something wrong'
-        }, { status: 400 });
-    }
-
-    const spited = decryptedData.split('+++');
-
-    console.log(spited);
-
-    return NextResponse.json({
-        decryptedData
-    }, { status: 400 });
-
-
 }
