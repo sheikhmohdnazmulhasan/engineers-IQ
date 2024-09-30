3d8a4ea76c106c721051c8c2bf334ff62c6daedc
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-09-30 19:01:15 +0600

    Handling loading state of sending account verification email

--- app/(others_layout)/auth/verify/page.tsx
+++ app/(others_layout)/auth/verify/page.tsx
@@ -3,38 +3,50 @@
 import { Button, Card } from "@nextui-org/react";
 import { useSearchParams } from "next/navigation";
 import { toast } from "sonner";
+import { useState } from "react";
 
 import sendAccountVerificationEmail from "@/utils/send_account_verification_email";
+import Loading from "@/components/loading";
 const NextStep = () => {
     const searchParams = useSearchParams();
     const email = searchParams.get('email');
+    const [isLoading, setIsLoading] = useState<boolean>(false)
 
     const handleResendEmail = async () => {
+        setIsLoading(true)
+
         if (email) {
             const emailRes = await sendAccountVerificationEmail({ email });
+            if (emailRes?.status === 200) {
+                toast.success('Email Send. Plz Check Your Inbox and Spam Folder');
+                setIsLoading(false)
+            } else {
+                toast.error('Failed To Resend Email. Try Again!')
+                setIsLoading(false)
+            };
 
         } else {
             toast.error('Something Went Wrong!')
+            setIsLoading(false);
         }
     };
 
-    const handleSkip = () => {
-        console.log(process.env.NEXT_PUBLIC_MONGODB_CONNECTION_STRING);
-    };
-
     return (
-        <div className="h-screen flex justify-center items-center fixed inset-0 px-5">
-            <Card className="max-w-lg mx-auto p-5">
-                <h1 className="text-2xl mb-">Verify Your Email</h1>
-                <p className="mb-4">Please check your email for a verification link. You must verify your email to activate your account.</p>
-                <p className="">We have sent a verification link to: <b>{email}</b></p>
+        <>
+            {isLoading && <Loading />}
+            <div className="h-screen flex justify-center items-center fixed inset-0 px-5">
+                <Card className="max-w-lg mx-auto p-5">
+                    <h1 className="text-2xl mb-">Verify Your Email</h1>
+                    <p className="mb-4">Please check your email for a verification link. You must verify your email to activate your account.</p>
+                    <p className="">We have sent a verification link to: <b>{email}</b></p>
 
-                <div className="space-y-4 mt-5">
-                    <Button className="w-full" onClick={handleResendEmail}>Resend Email</Button>
-                    <Button className="w-full" color="secondary" variant="flat" onClick={handleSkip}>Skip for Now</Button>
-                </div>
-            </Card>
-        </div>
+                    <div className="space-y-4 mt-5">
+                        <Button className="w-full" onClick={handleResendEmail}>Resend Email</Button>
+                        <Button className="w-full" color="secondary" variant="flat" >Skip for Now</Button>
+                    </div>
+                </Card>
+            </div>
+        </>
     );
 };
 
