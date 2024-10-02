db093d2beafec7dfb6e3a3c6ef4d45166e16e0af
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-02 23:35:47 +0600

    10-2-24

--- config/site.ts
+++ config/site.ts
@@ -9,20 +9,16 @@ export const siteConfig = {
       href: "/",
     },
     {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
       label: "Pricing",
       href: "/pricing",
     },
     {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
       label: "About",
       href: "/about",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
     },
   ],
 };
