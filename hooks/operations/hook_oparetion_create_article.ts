fc306c40c24181a04e0d4c29a93b5cce5e16838e
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-04 01:14:18 +0600

    useCreateArticle hook created with tansstack

---
+++ hooks/operations/hook_oparetion_create_article.ts
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { FieldValues } from "react-hook-form";
+import { toast } from "sonner";
+
+import { createArticle } from "@/services/article.services";
+
+export async function useCreateArticle(username: string) {
+    const router = useRouter();
+
+    return useMutation({
+        mutationKey: ['ARTICLE_CREATION'],
+        mutationFn: async (payload: FieldValues) => await createArticle(payload),
+        onSuccess: () => {
+            router.push(`/profile/${username}`);
+        },
+        onError: (error) => {
+            toast.error(error.message);
+        }
+    })
+}
