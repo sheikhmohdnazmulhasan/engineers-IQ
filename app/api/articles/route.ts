c94991ade22ef89487f0d5d1037730c8bd222a2a
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-03 13:51:21 +0600

    endpoint created for creting new article

--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -1,10 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import httpStatus from "http-status";
 import { NextResponse } from "next/server";
+
+import Article from "@/models/article.model";
 
 // Creating new article
 export async function POST(request: Request) {
     const data = await request.json();
 
-    console.log(data);
+    try {
+        const result = await Article.create(data);
 
-    return NextResponse.json("hello")
+        if (result) {
+            return NextResponse.json({
+                success: true,
+                message: 'Article Published',
+                data: result
+            }, { status: httpStatus.CREATED });
+        }
+
+        return NextResponse.json({
+            success: false,
+            message: 'Failed to publish the article'
+        }, { status: httpStatus.BAD_REQUEST });
+
+    } catch (error: any) {
+        return NextResponse.json({
+            success: false,
+            message: 'An error occurred while creating the article',
+            error: error.message
+        }, { status: httpStatus.INTERNAL_SERVER_ERROR });
+    }
 }
