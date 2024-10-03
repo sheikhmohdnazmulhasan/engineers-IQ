41ec6ca6a1ea54b01536ce8c227074afd4f8d9f6
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-03 21:43:27 +0600

    article publishing form modfd

--- app/(main_layout)/new/page.tsx
+++ app/(main_layout)/new/page.tsx
@@ -3,7 +3,7 @@
 import React, { useRef } from 'react'
 import { Editor, IAllProps } from '@tinymce/tinymce-react'
 import { Input } from '@nextui-org/input';
-import { Select, SelectItem } from '@nextui-org/react';
+import { Button, Checkbox, Select, SelectItem } from '@nextui-org/react';
 
 import { categoriesData } from '@/const/article/categories';
 import { topicsData } from '@/const/article/topics';
@@ -27,15 +27,14 @@ export default function PostEditor() {
     }
 
     return (
-        <div className="space-y-4">
+        <form className="space-y-4">
             <h1 className='text-3xl'>Publish New Article</h1>
             <p>EngineersIQ is more than just a platform; it&apos;s a thriving community of engineers, professionals, and curious learners. Our diverse user base contributes to a rich ecosystem of knowledge, where everyone has something to learn and something to teach.</p>
-            <form className='pt-5 space-y-4'>
+            <div className='pt-5 space-y-4'>
                 <Input isRequired label='Title' size='sm' type='text' />
                 <div className=" md:flex gap-4 space-y-4 md:space-y-0">
                     <Select
                         isRequired
-                        selectionMode='multiple'
                         items={categoriesData}
                         label="Category"
                         size='sm'
@@ -46,12 +45,13 @@ export default function PostEditor() {
                         isRequired
                         items={topicsData}
                         label="Select Topics"
+                        selectionMode='multiple'
                         size='sm'
                     >
                         {(topic) => <SelectItem key={topic.key}>{topic.label}</SelectItem>}
                     </Select>
                 </div>
-            </form>
+            </div>
             <Editor
                 apiKey={process.env.NEXT_PUBLIC_TINYMCA_API_KEY}
                 init={{
@@ -70,12 +70,13 @@ export default function PostEditor() {
                 }}
                 onInit={handleEditorInit}
             />
-            <button
-                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
-                onClick={handleSave}
-            >
-                Save
-            </button>
-        </div>
+            <div className="flex justify-between">
+                <Checkbox>Publish Premium</Checkbox>
+                <div className="flex gap-4">
+                    <Button color="primary" variant="bordered">Save Draft</Button>
+                    <Button color="primary">Publish Now</Button>
+                </div>
+            </div>
+        </form>
     )
 }
