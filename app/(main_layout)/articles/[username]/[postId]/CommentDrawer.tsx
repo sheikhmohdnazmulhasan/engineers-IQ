800c835544bc42b42988a65605b847d6972b1e7d
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-05 09:08:31 +0600

    Edit and delete buttons are shown on own comments

--- app/(main_layout)/articles/[username]/[postId]/CommentDrawer.tsx
+++ app/(main_layout)/articles/[username]/[postId]/CommentDrawer.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Heart, X, } from 'lucide-react'
+import { Heart, X, Edit, Trash } from 'lucide-react'
 import { Button, Textarea, Avatar, } from "@nextui-org/react"
 import { AnimatePresence, motion, } from 'framer-motion'
 import { toast } from 'sonner'
@@ -23,10 +23,6 @@ export const CommentDrawer: React.FC<CommentDrawerProps> = ({ isOpen, onClose, c
     const { currentUser } = useUser();
     const [newComment, setNewComment] = useState('');
     const [loading, setLoading] = useState<boolean>(false);
-
-    const hasClapped = comments?.some((comment: IComment) =>
-        comment.claps.some((clap: IClap) => clap._id === currentUser?._id)
-    );
 
     const handleSubmitComment = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -53,6 +49,8 @@ export const CommentDrawer: React.FC<CommentDrawerProps> = ({ isOpen, onClose, c
             setLoading(false);
         }
     }
+
+    // console.log(hasClapped);
 
     useEffect(() => {
         if (isOpen) {
@@ -101,32 +99,68 @@ export const CommentDrawer: React.FC<CommentDrawerProps> = ({ isOpen, onClose, c
                                 <Button color="primary" isLoading={loading} type="submit" variant='bordered'>Respond</Button>
                             </form>
                             <div className="space-y-4">
-                                {comments.slice().reverse().map((comment, index) => (
-                                    <motion.div
-                                        key={index}
-                                        animate={{ opacity: 1, y: 0 }}
-                                        className="flex space-x-3"
-                                        initial={{ opacity: 0, y: 20 }}
-                                        transition={{ delay: index * 0.1 }}
-                                    >
-                                        <Avatar name={comment.user.name} src={comment.user.profileImg} />
-                                        <div className="flex-grow">
-                                            <UserName isPremium={comment.user.isPremiumMember} name={comment.user.name} />
-                                            {/* <p className="font-semibold">{comment.user.name}</p> */}
-                                            <p className="text-sm text-gray-500">{formatDateReadable(comment.createdAt)}</p>
-                                            <p className="mt-1">{comment.content}</p>
-                                            <Button
-                                                className={`mt-2 p-0 ${hasClapped ? 'text-danger' : 'text-gray-500'}`}
-                                                size="sm"
-                                                startContent={<Heart className={`w-4 h-4 ${hasClapped ? 'fill-current text-danger' : ''}`} />}
-                                                variant="light"
-                                                onPress={() => onLikeComment(comment._id)}
-                                            >
-                                                {comment.claps.length}
-                                            </Button>
-                                        </div>
-                                    </motion.div>
-                                ))}
+                                {comments.slice().reverse().map((comment, index) => {
+                                    const hasClapped = comment.claps.some((xx: IClap) => xx._id === currentUser?._id);
+                                    const hasWonComment = comment.user._id === currentUser?._id
+
+                                    return (
+                                        <motion.div
+                                            key={index}
+                                            animate={{ opacity: 1, y: 0 }}
+                                            className="flex space-x-3"
+                                            initial={{ opacity: 0, y: 20 }}
+                                            transition={{ delay: index * 0.1 }}
+                                        >
+                                            <Avatar name={comment.user.name} src={comment.user.profileImg} />
+                                            <div className="flex-grow">
+                                                <UserName isPremium={comment.user.isPremiumMember} name={comment.user.name} />
+                                                {/* <p className="font-semibold">{comment.user.name}</p> */}
+                                                <p className="text-sm text-gray-500">{formatDateReadable(comment.createdAt)}</p>
+                                                <p className="mt-1">{comment.content}</p>
+
+                                                {!hasWonComment ? <Button
+                                                    className={`mt-2 p-0 ${hasClapped ? 'text-danger' : 'text-gray-500'}`}
+                                                    size="sm"
+                                                    startContent={<Heart className={`w-4 h-4 ${hasClapped ? 'fill-current text-danger' : ''}`} />}
+                                                    variant="light"
+                                                    onPress={() => onLikeComment(comment._id)}
+                                                >
+                                                    {comment.claps.length}
+                                                </Button>
+                                                    :
+                                                    <>
+                                                        <Button
+                                                            className={`mt-2 p-0 text-gray-500`}
+                                                            size="sm"
+                                                            startContent={<Heart className={`w-4 h-4`} />}
+                                                            variant="light"
+                                                            onPress={() => onLikeComment(comment._id)}
+                                                        >
+                                                            {comment.claps.length}
+                                                        </Button>
+
+                                                        <Button
+                                                            isIconOnly
+                                                            className={`mt-2 p-0 text-gray-500`}
+                                                            size="sm"
+                                                            startContent={<Edit className={`w-4 h-4`} />}
+                                                            variant="light"
+                                                            onPress={() => onLikeComment(comment._id)}
+                                                        />
+                                                        <Button
+                                                            isIconOnly
+                                                            className={`mt-2 p-0 text-danger`}
+                                                            size="sm"
+                                                            startContent={<Trash className={`w-4 h-4`} />}
+                                                            variant="light"
+                                                            onPress={() => onLikeComment(comment._id)}
+                                                        />
+                                                    </>}
+
+                                            </div>
+                                        </motion.div>
+                                    )
+                                })}
                             </div>
                         </div>
                     </motion.div>
