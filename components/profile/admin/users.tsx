3dff9483f54c6414e196ce7776dc5fbc6bebeea9
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-08 13:34:28 +0600

    user data mapped in admin dashboard

--- components/profile/admin/users.tsx
+++ components/profile/admin/users.tsx
@@ -1,33 +1,64 @@
 import { DeleteIcon, EditIcon, EyeIcon } from "@/components/icons";
 import useAllUsers from "@/hooks/use_all_users";
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Spinner, Chip } from "@nextui-org/react";
 
 export default function Users() {
+    const { data, error, isLoading } = useAllUsers();
 
-    const { data } = useAllUsers();
+    // Handle error state
+    if (error) {
+        return (
+            <div className="h-screen flex justify-center flex-col items-center -mt-32">
+                <h2 className="text-center">Failed to load users. Please try again!</h2>
+            </div>
+        );
+    }
 
-    console.log(data);
+    // Handle loading state
+    if (isLoading) {
+        return (
+            <div className="h-screen flex justify-center flex-col items-center -mt-32">
+                <Spinner size="lg" />
+            </div>
+        );
+    }
 
-    return (
-        <Table removeWrapper aria-label="Example static collection table">
-            <TableHeader>
-                <TableColumn>NAME</TableColumn>
-                <TableColumn>ROLE</TableColumn>
-                <TableColumn>STATUS</TableColumn>
-                <TableColumn>ACTIONS</TableColumn>
-            </TableHeader>
-            <TableBody>
-                <TableRow key="1">
-                    <TableCell>William Howard</TableCell>
-                    <TableCell>Community Manager</TableCell>
-                    <TableCell>Community Manager</TableCell>
-                    <TableCell className="flex gap-3">
-                        <EyeIcon />
-                        <EditIcon />
-                        <DeleteIcon />
-                    </TableCell>
-                </TableRow>
-            </TableBody>
-        </Table>
-    );
+    // Handle empty state
+    if (!data || data.data?.length === 0) {
+        return (
+            <div className="h-screen flex justify-center flex-col items-center -mt-32">
+                <h2 className="text-center">There are no users to display!</h2>
+            </div>
+        );
+    };
+
+    // render actual content
+    if (data.data?.length) {
+        return (
+            <Table removeWrapper aria-label="Users table">
+                <TableHeader>
+                    <TableColumn>NAME</TableColumn>
+                    <TableColumn>ROLE</TableColumn>
+                    <TableColumn>STATUS</TableColumn>
+                    <TableColumn>ACTIONS</TableColumn>
+                </TableHeader>
+                <TableBody>
+                    {data.data?.map((user, index) => (
+                        user && (
+                            <TableRow key={user.id || index}>
+                                <TableCell>{user.name || "N/A"}</TableCell>
+                                <TableCell>{user.role.charAt(0).toUpperCase() + user.role.slice(1) || "N/A"}</TableCell>
+                                <TableCell>{user.isBlocked ? <Chip size="sm" color="danger">Blcked</Chip> : <Chip size='sm' color="primary">Active</Chip>}</TableCell>
+                                <TableCell className="flex gap-3">
+                                    <EyeIcon />
+                                    <EditIcon />
+                                    <DeleteIcon />
+                                </TableCell>
+                            </TableRow>
+                        )
+                    ))}
+                </TableBody>
+            </Table>
+        )
+    }
 }
